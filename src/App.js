efcebd7d189e1aeec922852d9f81e4f655dc7b50
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Air.local>
2024-04-22 04:01:02 -0700

    deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -8,27 +8,7 @@ import AddTask from './components/AddTask';
 function App() {
   // adding a boolean useState, shows 'showAddTask' by default, we want button to toggle add
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 5th at 1:30pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false,
-        },
-    ])
+  const [tasks, setTasks] = useState('')
 
     // addTaskHandler()
     const addTaskHandler = ((task) => {
