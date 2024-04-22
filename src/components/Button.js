e02970c9d0500acc800fe431cd3790ac9b55211b
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Air.local>
2024-04-22 03:30:15 -0700

    deleted unnecessary files

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
 
     return (
         // calling onClick from header.js
-        <button onClick = {props.onClick} className='btn'>
+        <button onClick = {props.onClick} className='btn' style = {{backgroundColor: props.color}}>
             {props.text}
         </button>
     )
